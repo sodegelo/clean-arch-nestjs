88dea6e9d8f70bccca2204e970079d629d036c11
Julio Resende <julioalbertoresende@gmail.com>
2022-06-17 14:54:13 -0300

    Ajuste

--- src/routes/routes.module.ts
+++ src/routes/routes.module.ts
@@ -4,12 +4,25 @@ import { RoutesController } from './routes.controller';
 import { RouteDbRepository } from 'src/@core/infra/db/memory/route-db.repository';
 import { CreateRouteUseCase } from 'src/@core/application/create-route.use-case';
 import { RouteRepositoryInterface } from 'src/@core/domain/route.repository';
+import { Route  } from 'src/@core/domain/route.entity';
 import { ListRoutesUseCase } from 'src/@core/application/list-routes.use-case';
+import { TypeOrmModule, getDataSourceToken } from '@nestjs/typeorm';
+import { RouteSchema } from 'src/@core/infra/db/typeorm/route.schema';
+import { RouteTypeOrmRepository } from 'src/@core/infra/db/typeorm/route-typeorm.repository';
+import { DataSource } from 'typeorm';
 
 @Module({
+  imports: [ TypeOrmModule.forFeature([RouteSchema])],
   controllers: [RoutesController],
   providers: [
     RoutesService,
+    {
+      provide:  RouteTypeOrmRepository,
+      useFactory: (dataSource: DataSource)=>{
+        return new RouteTypeOrmRepository(dataSource.getRepository(Route));
+      }, 
+      inject: [getDataSourceToken()]
+    },
     {
       provide:  RouteDbRepository,
       useClass: RouteDbRepository
@@ -19,14 +32,14 @@ import { ListRoutesUseCase } from 'src/@core/application/list-routes.use-case';
       useFactory: (routeRepo: RouteRepositoryInterface)=>{
         return new CreateRouteUseCase(routeRepo);
       },
-      inject: [RouteDbRepository]
+      inject: [RouteTypeOrmRepository]
     },
     {
       provide:  ListRoutesUseCase,
       useFactory: (routeRepo: RouteRepositoryInterface)=>{
         return new ListRoutesUseCase(routeRepo);
       },
-      inject: [RouteDbRepository]
+      inject: [RouteTypeOrmRepository]
     }
   ]
 })
